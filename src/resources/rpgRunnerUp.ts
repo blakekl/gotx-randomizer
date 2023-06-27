c15536ff47b6b4b294ca22036e96ed85364ef84b
blakekl <blakekl10@gmail.com>
2023-06-27 16:38:27 -0600

    Added the current contenders in seperate object for rpgoq runner ups.

--- src/resources/rpgRunnerUp.ts
+++ src/resources/rpgRunnerUp.ts
@@ -1,5 +1,52 @@
 import { GameData } from '../models/gameData.model';
 
+const current: GameData[] = [
+  {
+    img: 'https://screenscraper.fr/image.php?gameid=5791&media=ss&region=wor&num=&version=&maxwidth=640&maxheight=480',
+    title: 'Final Fantasy Tactics A2: Grimoire of the Rift',
+    year: 2007,
+    system: 'Nintendo DS',
+    developer: 'Square Enix',
+    genre: 'TRPG',
+    timeToBeat: 49,
+    description:
+      'The third (and so far, final) Tactics spin off for the Final Fantasy series. This time, control Luso and his clan as he is magically transported to Ivalice seeking a way to return home.',
+  },
+  {
+    img: 'https://screenscraper.fr/image.php?gameid=19513&media=ss&region=wor&num=&version=&maxwidth=640&maxheight=480',
+    title: 'Wild Arms 2',
+    year: 1999,
+    system: 'Playstation 1',
+    developer: 'MediaVision, Contrail',
+    genre: 'JRPG',
+    timeToBeat: 42,
+    description:
+      'When a terrorist organization called Odessa tries to conquer the world, take control of an international anti-terrorist group called ARMS to combat them.',
+  },
+  {
+    img: 'https://screenscraper.fr/image.php?gameid=117305&media=ss&region=jp&num=&version=&maxwidth=640&maxheight=480',
+    title: 'Racing Lagoon',
+    year: 1999,
+    system: 'Playstation 1',
+    developer: 'Square',
+    genre: 'Racing RPG',
+    timeToBeat: 32,
+    description:
+      "Take to the streets in this unique racing RPG, which focuses on eam Bay Lagoon Racing's newest recruit, Sho Akasaki, who is determined to learn the truth behind the fabled Fastest Legend race.",
+  },
+  {
+    img: 'https://screenscraper.fr/image.php?gameid=19292&media=ss&region=wor&num=&version=&maxwidth=640&maxheight=480',
+    title: 'Valkyrie Profile',
+    year: 1999,
+    system: 'Playstation 1',
+    developer: 'tri-ace',
+    genre: 'JRPG',
+    timeToBeat: 31,
+    description:
+      'Inspired by Norse mythology control Lenneth, as she travels through Midgard, collecting souls of slain heroes to serve her either as einherjar, or her personal companions.',
+  },
+];
+
 export const rpgRunnerUp: GameData[] = [
   {
     img: 'https://cdn.mobygames.com/screenshots/15992482-infinite-space-nintendo-ds-going-back-a-bit-unsure-how-that-work.png',
