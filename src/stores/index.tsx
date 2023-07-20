4932e17f531b794611c12a082eacb33f3c89e463
blakekl <blakekl10@gmail.com>
2023-07-20 15:02:27 -0600

    Removed the unused DatabaseStore

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -1,8 +1,6 @@
 import { createContext } from 'react';
-import DatabaseStore from './DatabaseStore';
 import RandomizerStore from './RandomizerStore';
 
 export default createContext({
-  databaseStore: new DatabaseStore(),
   randomizerStore: new RandomizerStore(),
 });
