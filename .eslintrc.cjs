4f3677b8d7334725c7656215d140b76488a28b26
Blake Longmore <blongmore@ancestry.com>
2023-08-15 16:29:17 -0600

    More linter changes.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
     // 'plugin:@typescript-eslint/strict-type-checked',
     "plugin:react-hooks/recommended",
+    `plugin:react/recommended`,
+    `plugin:react/jsx-runtime`
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
