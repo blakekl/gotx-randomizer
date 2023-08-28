f35931697b50494f94aa187e34c38a0b88f9b27e
Blake Longmore <blongmore@ancestry.com>
2023-08-28 11:12:06 -0600

    Utilizes the sqlite file for the database rather than executing all the database creation SQL statements on the client. Should speed loading quite a bit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
+  plugins: [react(), tsconfigPaths(), nodePolyfills({
+    exclude: [
+      "assert",
+      "child_process",
+      "console",
+      "constants",
+      "crypto",
+      "dgram",
+      "dns",
+      "events",
+      "fs",
+      "http",
+      "http2",
+      "https",
+      "net",
+      "os",
+      "path",
+      "process",
+      "querystring",
+      "readline",
+      "repl",
+      "stream",
+      "string_decoder",
+      "timers",
+      "tls",
+      "tty",
+      "url",
+      "util",
+      "vm",
+      "zlib"
+  ],
+    globals: {
+      Buffer: true,
+    }
+  })],
+  assetsInclude: ['**/*.sqlite'],
 });
