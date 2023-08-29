4356d31695a9ca18c384176829392fbc0b96dd18
LiquidDivide <blakekl10@gmail.com>
2023-08-29 10:52:07 -0600

    Adds a button to hide the current game. (#182)

--- src/pages/Randomizer/Randomizer.tsx
+++ src/pages/Randomizer/Randomizer.tsx
@@ -1,48 +1,71 @@
-import { useMediaQuery } from 'react-responsive';
 import { observer } from 'mobx-react-lite';
 import classNames from 'classnames';
 import Settings from './Settings';
-import GameDisplay from './GameDisplay';
+import GameDisplay from './GameDisplay/GameDisplay';
 import { useStores } from '../../stores/useStores';
 import { useState } from 'react';
 
 const Randomizer = observer(() => {
   const { randomizerStore } = useStores();
-  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
   const [imgLoaded, setImgLoaded] = useState(false);
 
-  const handleButtonClick = () => {
+  const rollButtonClick = () => {
     setImgLoaded(false);
     randomizerStore.nextGame();
   };
 
-  return (
+  const handleToggleClick = () => {
+    randomizerStore.toggleGameHidden();
+    rollButtonClick();
+  };
+
+  const noGame = (
     <>
       <Settings />
-      <div className="has-text-centered">
+      <div className="notification mt-6 has-text-centered">
+        There are no games left in the pool. They have either all been hidden, or
+        you have removed all game types in settings. Please check settings and try again.
+      </div>
+    </>
+  );
+  
+  const hasGame = (
+    <>
+      <Settings />
+      <div className="mt-4 buttons has-addons is-centered">
         <button
           className={classNames({
             button: true,
             'is-primary': true,
             rollBtn: true,
-            'is-fullwidth': isMobile,
-            'is-large': !isMobile,
           })}
-          onClick={() => {
-            handleButtonClick();
-          }}
+          onClick={rollButtonClick}
+          disabled={randomizerStore.filteredGamePool.length < 2}
         >
-          {imgLoaded && <span>Reroll</span>}
+          <span>Reroll</span>
           {!imgLoaded && (
             <span className="icon is-small">
               <span className="loader"></span>
             </span>
           )}
         </button>
+        <button className={classNames({
+            'button': true,
+            'is-danger': !randomizerStore.isGameHidden,
+            'is-success': randomizerStore.isGameHidden
+          })} onClick={handleToggleClick}>
+          { randomizerStore.isGameHidden ? 'Unhide Game' : 'Hide Game' }
+        </button>
       </div>
       <GameDisplay imgLoaded={imgLoaded} setImgLoaded={setImgLoaded} />
     </>
   );
+
+  if (randomizerStore.currentGame.id > 0) {
+    return hasGame;
+  } else {
+    return noGame;
+  }
 });
 
 export default Randomizer;
--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,53 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {nodePolyfills} from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
     supported: {
-      'top-level-await': true //browsers can handle top-level-await features
+      'top-level-await': true, //browsers can handle top-level-await features
     },
   },
-  plugins: [react(), tsconfigPaths(), nodePolyfills({
-    exclude: [
-      "assert",
-      "child_process",
-      "console",
-      "constants",
-      "crypto",
-      "dgram",
-      "dns",
-      "events",
-      "fs",
-      "http",
-      "http2",
-      "https",
-      "net",
-      "os",
-      "path",
-      "process",
-      "querystring",
-      "readline",
-      "repl",
-      "stream",
-      "string_decoder",
-      "timers",
-      "tls",
-      "tty",
-      "url",
-      "util",
-      "vm",
-      "zlib"
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    nodePolyfills({
+      exclude: [
+        'assert',
+        'child_process',
+        'console',
+        'constants',
+        'crypto',
+        'dgram',
+        'dns',
+        'events',
+        'fs',
+        'http',
+        'http2',
+        'https',
+        'net',
+        'os',
+        'path',
+        'process',
+        'querystring',
+        'readline',
+        'repl',
+        'stream',
+        'string_decoder',
+        'timers',
+        'tls',
+        'tty',
+        'url',
+        'util',
+        'vm',
+        'zlib',
+      ],
+      globals: {
+        Buffer: true,
+      },
+    }),
   ],
-    globals: {
-      Buffer: true,
-    }
-  })],
   assetsInclude: ['**/*.sqlite'],
 });
