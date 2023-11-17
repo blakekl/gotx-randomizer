2354aacf625f449e6152ea3d6a57e6f2a8945e6f
Blake Longmore <blakekl10@gmail.com>
2023-11-17 15:12:43 -0700

    Add some verbosity to the deploy script.

--- deploy.js
+++ deploy.js
@@ -4,8 +4,11 @@ const dest = 'ftp-retrohandhelds:randomizer.retrohandhelds.gg';
 
 (async () => {
   try {
+    console.log('Uploading new files');
     await rclone.promises.copy(source, dest); // upload any new files
+    console.log('Teardown old files');
     await rclone.promises.sync(source, dest); // delete's any removed files once the upload succeeds.
+    console.log('Success!');
   } catch (e) {
     console.error('failed to upload files.');
     process.exit(1);
