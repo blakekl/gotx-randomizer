ff0f2973a3035dfd89d115ce319f0d9943113260
Blake Longmore <blakekl10@gmail.com>
2023-11-29 10:01:57 -0700

    Centered text on 404 page.

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,8 @@
 const NotFound = () => {
     return <>
         <h1 className="title has-text-centered">404 Not Found</h1>
-        <p>You have exited the realm of random and entered pure chaos. Please navigate to an existing page.</p>
+        <p className="has-text-centered">You have exited the realm of random and entered pure chaos.</p>
+        <p className="has-text-centered">Please navigate to an existing page.</p>
     </>
 }
 export default NotFound
