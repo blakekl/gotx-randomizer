ca206bfce07ca2964aaacfde42d6a7d8fed6f11c
Blake Longmore <blakekl10@gmail.com>
2024-03-21 15:00:29 -0600

    Use DUMP_PATH in sed commands.

--- scripts/gotxDbDump.sh
+++ scripts/gotxDbDump.sh
@@ -87,21 +87,21 @@ CREATE TABLE [public.streaks] (
 ssh -C $SERVER_NAME@$SERVER_HOST -p $PORT  pg_dump --data-only --inserts -U $DB_USER -h localhost $DATABASE >> $DUMP_PATH
 
 # Replace invaled sqlite statements.
-sed -i 's/INTO public.completions/INTO [public.completions]/' dump.sql
-sed -i 's/INTO public.games/INTO [public.games]/' dump.sql
-sed -i 's/INTO public.nominations/INTO [public.nominations]/' dump.sql
-sed -i 's/INTO public.streaks/INTO [public.streaks]/' dump.sql
-sed -i 's/INTO public.themes/INTO [public.themes]/' dump.sql
-sed -i 's/INTO public.users/INTO [public.users]/' dump.sql
-sed -i 's/id bigint NOT NULL/id integer primary key autoincrement/' dump.sql
-sed -i 's/bigint/int/' dump.sql
-sed -i 's/, true,/, 1,/' dump.sql 
-sed -i 's/, false,/, 0,/' dump.sql
+sed -i 's/INTO public.completions/INTO [public.completions]/' $DUMP_PATH
+sed -i 's/INTO public.games/INTO [public.games]/' $DUMP_PATH
+sed -i 's/INTO public.nominations/INTO [public.nominations]/' $DUMP_PATH
+sed -i 's/INTO public.streaks/INTO [public.streaks]/' $DUMP_PATH
+sed -i 's/INTO public.themes/INTO [public.themes]/' $DUMP_PATH
+sed -i 's/INTO public.users/INTO [public.users]/' $DUMP_PATH
+sed -i 's/id bigint NOT NULL/id integer primary key autoincrement/' $DUMP_PATH
+sed -i 's/bigint/int/' $DUMP_PATH
+sed -i 's/, true,/, 1,/' $DUMP_PATH 
+sed -i 's/, false,/, 0,/' $DUMP_PATH
 
 # DELETE postgres specific goo.
-sed -i '/^--/d' dump.sql
-sed -i '/public.schema_migrations/d' dump.sql
-sed -i '/SELECT pg_catalog/d' dump.sql
-sed -i '/INSERT INTO public.ar_internal_metadata/d' dump.sql
-sed -i '/^SET/d' dump.sql
-sed -i '/^SELECT pg_catalog.setval/d' dump.sql 
+sed -i '/^--/d' $DUMP_PATH
+sed -i '/public.schema_migrations/d' $DUMP_PATH
+sed -i '/SELECT pg_catalog/d' $DUMP_PATH
+sed -i '/INSERT INTO public.ar_internal_metadata/d' $DUMP_PATH
+sed -i '/^SET/d' $DUMP_PATH
+sed -i '/^SELECT pg_catalog.setval/d' $DUMP_PATH 
