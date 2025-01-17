04f434858ddde9b4bbf6ed18945c71db0b1791bf
Blake Longmore <blakekl10@gmail.com>
2025-01-17 16:16:12 -0700

    Fixed the layout of the homepage to be how I really want it.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,9 +4,18 @@ const Home = () => {
   return (
     <>
       <h1 className="title is-1 has-text-centered">GotX Dashboard</h1>
-      <div className="fixed-grid has-1-cols-mobile has-2-cols">
+      <p>
+        Welcome to the GotX Dashboard. Your place for all the details on the
+        Retro Handhelds GotX program. Select a tool from the list below. Use
+        Randomizer to find a random game featured in GotX, one way or another.
+        Use stats to see what&apos;s being nominated, played, and other data facts.
+        Use games to search for a particular game. And finally, use users to see
+        your own personal (or anyone else&apos;s) completed games and nomination
+        lists.
+      </p>
+      <div className="fixed-grid mt-6 has-1-cols-mobile has-2-cols-tablet has-4-cols-desktop">
         <div className="grid is-size-3 has-text-centered">
-          <div className="cell is-col-min-2">
+          <div className="cell">
             <Link to="/randomizer">
               <div className="box">
                 <div className="fa-solid fa-shuffle" />
